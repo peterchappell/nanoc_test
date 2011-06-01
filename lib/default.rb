ed8276b625a797498e9fb80179c4c7d1ae6cb8eb
Peter Chappell <pete@skinny.local>
2011-06-01 18:30:57 +1000

    Added file/folder structure and table of contents and breadcrumbs

--- lib/default.rb
+++ lib/default.rb
@@ -1,5 +1,9 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
 
+require 'lib/helpers/toc'
+
 # Helpers
 include Nanoc3::Helpers::LinkTo
+include Nanoc3::Helpers::Breadcrumbs
+include TocHelper
---
+++ lib/helpers/toc.rb
@@ -0,0 +1,34 @@
+module TocHelper
+  def toc (page, indentation=0, sitemap_buffer='')
+
+        # Open list element
+        sitemap_buffer << "\t" * indentation + '<li>'
+
+        # Add link
+        sitemap_buffer << link_to_unless_current(page[:title], page.reps[0])
+
+        # Add children to sitemap, recursively
+        visible_children = page.children.sort_by { |page| page[:title].downcase }
+        if visible_children.size > 0
+
+          # Open list
+          sitemap_buffer << "\t" * indentation + '<ul>' + "\n"
+
+          # Add children
+          visible_children.each do |child|
+            toc(child, indentation+1, sitemap_buffer)
+          end
+
+          # Close list
+          sitemap_buffer << "\t" * indentation + '</ul>' + "\n"
+
+        end
+
+        # Close list element
+        sitemap_buffer << '</li>'
+
+    # Return sitemap
+    sitemap_buffer
+
+  end
+end
